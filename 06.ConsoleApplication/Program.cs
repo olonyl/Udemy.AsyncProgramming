341d616091eb3b83517df28b60b3ed6182647365
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-04 15:39:02 -0600

    Handling errors.

---
+++ 06.ConsoleApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace _06.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var thread = new Thread(
+                () =>
+                {
+                    try
+                    {
+                        throw new Exception(
+                       "thread that crashed: " + Thread.CurrentThread.Name
+                       );
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                });
+            thread.Name = "Named Thread";
+
+            thread.Start();
+
+            var background = new Thread(() => throw new InvalidOperationException("nothing shown"))
+            {
+                Name = "silently fails"
+            };
+
+            background.Start();
+
+            Console.Read();
+        }
+    }
+}
