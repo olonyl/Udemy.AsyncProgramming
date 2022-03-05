fbf73220c2198b2cd9bd8598d07cc1286fd5f791
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-04 19:40:10 -0600

    Use of ThreadPool and TPL

---
+++ 07.ConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // "normal" thread
+            var thread = new Thread(() => RunOnThreadpool("Normal-Thread"));
+
+            // 1 Use ThreadPool Enqueuing
+            ThreadPool.QueueUserWorkItem(param => RunOnThreadpool("Queued Item"));
+
+            // 2 Task (chapter 4)
+            Task.Run(() => RunOnThreadpool("From the task"));
+
+            // 3 APM is another one, but obsolete
+            // usually BeginInvove and EndInvoke methods
+            thread.Start();
+
+
+            Console.Read();
+
+
+        }
+
+        private static void RunOnThreadpool(string threadName)//not allowed on thread pool
+        {
+            // hot to find out? --> Thread Pool thread is automatic background thread!
+            bool isBackground = Thread.CurrentThread.IsBackground;
+
+            string background = isBackground ? "a background thread" : "a foreground thread";
+            Console.WriteLine($"Now running thread is {background} with name {threadName}");
+        }
+    }
+}
