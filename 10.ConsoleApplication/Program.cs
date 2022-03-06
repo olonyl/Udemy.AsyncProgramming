e3488c52501a997f0078850ccfcd081a60bc7dee
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-06 09:55:21 -0600

    Using Semaphore

---
+++ 10.ConsoleApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace _10.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NonExclusive.RunClub();
+            Console.Read();
+        }
+    }
+
+    internal class NonExclusive
+    {
+        private static SemaphoreSlim _semaphore = new SemaphoreSlim(3);
+        public static void RunClub()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                new Thread(Enter).Start(i);
+            }
+        }
+
+        private static void Enter(object id)
+        {
+            Console.WriteLine($"{id} wants to enter");
+
+            _semaphore.Wait();
+            Console.WriteLine($"{id} walked in");
+            Thread.Sleep(1000 * (int)id);
+            Console.WriteLine($"{id} is leaving already");
+            _semaphore.Release();
+        }
+    }
+}
