af93c9561243ae2ad88f2eeef3ae83d5ae833465
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-07 20:16:37 -0600

    TaskCompletionSource

---
+++ 16.ConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _16.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            Task<int> synth = tcs.Task;
+            synth.ContinueWith(t => Console.WriteLine($"from synthetic task: {t.Result}"));
+            Console.WriteLine("waiting for a result!");
+            Console.ReadKey();
+
+            tcs.SetResult(42);
+            Console.ReadKey();
+
+
+        }
+    }
+}
