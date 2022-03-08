81f321ee766b6a3e2b25bd486c1ab796a6130e24
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-07 20:09:36 -0600

    Cancellation Token

---
+++ 15.ConsoleApplication/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _15.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tcs = new CancellationTokenSource();
+            CancellationToken token = tcs.Token;
+
+            Task task = RunAsync(token);
+            Thread.Sleep(2000);
+
+            try
+            {
+                tcs.Cancel();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"state of task is {task.Status}");
+            }
+            Console.Read();
+        }
+
+        private static Task RunAsync(CancellationToken token)
+        {
+            return Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Thread.Sleep(200);
+                    Console.WriteLine("not cancelled yet");
+                }
+                Console.WriteLine("was cancelled");
+                token.ThrowIfCancellationRequested();
+
+                //or
+                throw new OperationCanceledException("cancelled", token);
+            });
+        }
+    }
+}
