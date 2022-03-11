84f4273efc50feea6d78d6249c3245ba2aa3244d
Olonyl Landeros <jose87.landeros@gmail.com>
2022-03-10 18:44:36 -0600

    Added disposable watcher

--- 18.Deadlock/Program.cs
+++ 18.Deadlock/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _18.Deadlock
@@ -19,20 +22,81 @@ namespace _18.Deadlock
             //DelegateProcessor.Run();
 
             //Console.WriteLine(SyncApi.GetWebResource("https://google.com"));
-            Console.WriteLine("test01");
-            CallAsyncOperation();
-            Console.WriteLine("test02");
-            Console.WriteLine();
-            Console.WriteLine();
+            //Console.WriteLine("test01");
+            //CallAsyncOperation();
+            //Console.WriteLine("test02");
+            //Console.WriteLine();
+            //Console.WriteLine();
 
+            using (var watcher = new Watcher())
+            {
+                try
+                {
 
+                    Thread.Sleep(2000);
+                    throw new Exception();
+
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
             Console.Read();
+
         }
 
         private static async void CallAsyncOperation()
         {
             var result = await AsynApi.GetWebResource("https://google.com");
             Console.WriteLine(result);
+        }
+    }
+
+    internal class Watcher : IDisposable
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+        private readonly ILogger _logger = new Logger();
+
+        public Watcher()
+        {
+            _stopwatch.Start();
+            _logger.Log($"Process Started");
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            _logger.Log($"Elapsed Time {_stopwatch.ElapsedMilliseconds}ms");
+
+            bool ExceptionOccurred = Marshal.GetExceptionPointers() != IntPtr.Zero
+                            || Marshal.GetExceptionCode() != 0;
+            if (ExceptionOccurred)
+            {
+                try
+                {
+
+                }
+                catch (Exception ex)
+                {
+
+                    System.Diagnostics.Debug.WriteLine("We had an exception");
+                }
+            }
+        }
+    }
+
+    internal interface ILogger
+    {
+        void Log(string message);
+    }
+
+    internal class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 
